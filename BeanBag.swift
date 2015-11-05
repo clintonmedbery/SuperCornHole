b2daf5710f52df46f7c05eb6a7eca2ae738163bb
Clinton Medbery <clinton.medbery@thinkideally.com>
2015-11-05 00:52:28 -0500

    Throws now scale and rotate.

--- BeanBag.swift
+++ BeanBag.swift
@@ -36,8 +36,25 @@ class BeanBag : SKSpriteNode{
     
     func throwBag(impulseAmount: CGFloat){
         
-        self.physicsBody?.velocity = CGVectorMake(0,0)
-        self.physicsBody?.velocity = CGVectorMake(0, impulseAmount)
+
+        let landingY: CGFloat = impulseAmount * 10.0
+        let point: CGPoint = CGPoint(x: self.position.x, y: self.position.y + landingY)
+        
+        let moveAction:SKAction = SKAction.moveTo(point, duration: 2)
+        let scaleAction: SKAction = SKAction.scaleBy(2, duration: 1.0)
+        let rotateAction: SKAction = SKAction.rotateByAngle((impulseAmount * 10), duration: 2.0)
+        
+        moveAction.timingMode = .EaseOut
+        scaleAction.timingMode = .EaseIn
+        
+        self.runAction(rotateAction)
+        self.runAction(moveAction)
+        self.runAction(scaleAction) { () -> Void in
+            let scaleBackAction: SKAction = SKAction.scaleBy(0.25, duration: 1.0)
+            scaleAction.timingMode = .EaseOut
+            self.runAction(scaleBackAction)
+
+        }
         
         
     }
@@ -45,6 +62,7 @@ class BeanBag : SKSpriteNode{
     func placeBeanBagAtStart(){
         self.position.y = 16
         self.position.x = 1000
+        self.setScale(2)
     }
     
 }
